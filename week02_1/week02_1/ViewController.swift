fbd5c15b0aa16cb3bbe5783274323fd32ebf6d91
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-04-14 00:37:15 +0900

    2주차 실습 과제 세팅

---
+++ week02_1/week02_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week02_1
+//
+//  Created by 조영서 on 4/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
