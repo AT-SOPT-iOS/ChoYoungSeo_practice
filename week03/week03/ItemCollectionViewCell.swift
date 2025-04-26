d7b7a13960d563ba8dad93a9780603f1393db94f
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-04-26 17:40:51 +0900

    [FEAT] 3주차 실습과제 구현

---
+++ week03/week03/ItemCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCollectionViewCell.swift
+//  week03
+//
+//  Created by 조영서 on 4/26/25.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ week03/week03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week03
+//
+//  Created by 조영서 on 4/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
